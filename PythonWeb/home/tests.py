38d7c9da91085af02bf5d2b123802c4cb1252615
Quang Huy Che <chehuy97@gmail.com>
2021-03-10 21:56:31 +0700

    add pythonweb

---
+++ PythonWeb/home/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase, SimpleTestCase
+
+# Create your tests here.
+
+class SimpleTests(SimpleTestCase):
+    def test_homepage_status(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+        
