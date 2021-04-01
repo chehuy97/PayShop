1e9cd7579ccc158519b413ddae48a1e61e247f1f
Quang Huy Che <chehuy97@gmail.com>
2021-04-01 10:10:24 +0700

    add models django

--- PythonWeb/home/urls.py
+++ PythonWeb/home/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.showHomeView),
+    path('home/', views.showHomeView),
+    path('menu/', views.showMenuView),
+    path('shop/', views.showShopView),
+    path('news/', views.showNewsView),
+    path('signin/', views.showSigninView),
 ]
--- PythonWeb/home/views.py
+++ PythonWeb/home/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
+def showHomeView(request):
     return render(request, 'pages/home.html')
+def showMenuView(request):
+    return render(request, 'pages/menu.html')
+def showShopView(request):
+    return render(request, 'pages/shop.html')
+def showNewsView(request):
+    return render(request, 'pages/news.html')
+def showSigninView(request):
+    return render(request, 'pages/signin.html')
+
